d5ee050883444fbde472376c2ec4eccee2ab6e3c
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-08-03 09:31:55 -0500

    refactor: update buttons isLoaded logic

--- __tests__/components/buttons-component.ts
+++ __tests__/components/buttons-component.ts
@@ -17,31 +17,41 @@ export class ButtonsComponent {
         this.fundingSource = fundingSource;
     }
 
+    async isLoaded(): Promise<boolean> {
+        // wait for second render to complete
+        let frame = await $("iframe[class='component-frame visible']");
+        await frame.waitForDisplayed();
+
+        // reselect the frame to avoid the error "Could not switch frame, unknown id"
+        frame = await $("iframe[class='component-frame visible']");
+        await frame.waitForDisplayed();
+
+        return await frame.isDisplayed();
+    }
+
+    async switchToButtonsFrame(): Promise<void> {
+        const frameBody = await $("body[data-client-version]");
+
+        // first check and see if we are already in the frame
+        if (await frameBody.isDisplayed()) {
+            return;
+        }
+
+        // otherwise assume we are on the merchant page
+        if (!(await this.isLoaded())) {
+            throw new Error("Second render failed for the buttons component");
+        }
+
+        const frame = await $("iframe[class='component-frame visible']");
+        await browser.switchToFrame(frame);
+    }
+
     async click(): Promise<void> {
         await this.switchToButtonsFrame();
 
         // select a button by funding source
         const button = await $(`[data-funding-source="${this.fundingSource}"]`);
         await button.waitAndClick();
-    }
-
-    async switchToButtonsFrame(): Promise<void> {
-        const frameBody = await $("body[data-client-version]");
-
-        // already in the iframe so there's no need to switch
-        if (await frameBody.isDisplayed()) {
-            return;
-        }
-
-        // wait for second render to complete
-        let frame = await $("iframe[class='component-frame visible']");
-        await frame.waitForDisplayed();
-
-        // select a button by funding source
-        frame = await $("iframe[class='component-frame visible']");
-        await frame.waitForDisplayed();
-
-        await browser.switchToFrame(frame);
     }
 
     async switchToPopupFrame(): Promise<void> {
@@ -75,9 +85,11 @@ export class ButtonsComponent {
 
     async getText(): Promise<string> {
         await this.switchToButtonsFrame();
+
         const buttonTextElement = await $(
             `[data-funding-source="${this.fundingSource}"] .paypal-button-text`
         );
+
         return buttonTextElement.getText();
     }
 }
