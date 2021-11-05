24d843cb677721a146bf40a51c6c7cd7529c3938
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-11-05 11:47:57 -0500

    Use web component for the perf metrics table

--- docs/utils.js
+++ docs/utils.js
@@ -14,6 +14,7 @@ export function getOptionsFromQueryString() {
         "disable-funding",
         "disable-card",
         "integration-date",
+        "data-namespace",
         "sdkBaseURL",
     ];
 
@@ -27,40 +28,4 @@ export function getOptionsFromQueryString() {
     });
 
     return validOptions;
-}
-
-export function getSDKScriptPerformance() {
-    if (!window.performance) {
-        return;
-    }
-
-    const sdkScript = document.querySelector("script[src*='/sdk/js?']");
-    const src = sdkScript ? sdkScript.src : "https://www.paypal.com/sdk/js?";
-
-    const scriptPerformance = performance
-        .getEntriesByType("resource")
-        .find((item) => item.name.includes(src));
-
-    return scriptPerformance;
-}
-
-export function setInnerHTML(selector, value) {
-    var element = document.querySelector(selector);
-    if (!element) {
-        return;
-    }
-
-    element.innerHTML = value;
-}
-
-export function getPagePerformance() {
-    if (!window.performance) {
-        return;
-    }
-
-    const performanceTiming = window.performance.getEntriesByType("navigation");
-
-    if (performanceTiming && performanceTiming[0]) {
-        return performanceTiming[0];
-    }
 }
