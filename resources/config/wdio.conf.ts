754d1838a072bf26383e05e991d06ad2877e6958
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-11-29 20:21:04 -0600

    Remove mocha awesome reporter

--- resources/config/wdio.conf.ts
+++ resources/config/wdio.conf.ts
@@ -1,7 +1,3 @@
-// since wdio-mochawesome-reporter does not have types, we use ts-ignore to ignore type checks on that
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import * as mergeResults from "wdio-mochawesome-reporter/mergeResults";
 import * as dotenv from "dotenv";
 
 // load environment variables from .env file for local development
@@ -32,24 +28,6 @@ export const config = {
         ui: "bdd",
         timeout: 150000,
     },
-    reporters: [
-        [
-            "mochawesome",
-            {
-                outputDir: "./mocha-report",
-                outputFileFormat: function (opts: {
-                    cid: unknown;
-                    capabilities: unknown;
-                }): string {
-                    return `results-${opts.cid}.json`;
-                },
-            },
-        ],
-    ],
-    mochawesomeOpts: {
-        includeScreenshots: true,
-        screenshotUseRelativePath: true,
-    },
     afterTest: function (
         _test: Record<string, unknown>,
         _context: Record<string, unknown>,
@@ -76,8 +54,5 @@ export const config = {
             },
             true
         );
-    },
-    onComplete: function (): void {
-        mergeResults("./mocha-report", "results-*");
     },
 };
