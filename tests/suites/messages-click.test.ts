090202e3204a60045b5a77bb6c9674963e70f3aa
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2022-03-11 14:12:53 -0600

    fix: simplify assertion for credit modal (#12)

--- tests/suites/messages-click.test.ts
+++ tests/suites/messages-click.test.ts
@@ -14,9 +14,7 @@ describe("messages", () => {
 
         await paypalMessagesComponent.switchToModalFrame();
 
-        const h1 = await $("h1");
-        const h1Text = await h1.getText();
-
-        expect(h1Text).to.equal("Buy now, pay later");
+        const pageTitle = await browser.getTitle();
+        expect(pageTitle).to.equal("Buy Now, Pay Later - PayPal");
     });
 });
