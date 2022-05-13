77b4fdf405b6ca06cf09c38c8d3ec0d5fd54dcdd
Ravi Shekhar <ravi.shekhar00@gmail.com>
2022-05-13 16:00:43 -0500

    Safari tests

---
+++ resources/config/wdio.browserstack-safari.conf.ts
@@ -0,0 +1,18 @@
+import { config as defaultConfig } from "./wdio.browserstack.conf";
+import * as _ from "lodash";
+
+const overrides = {
+    maxInstances: 1,
+    services: ["safaridriver"],
+    specs: ["tests/**/*.test.ts"],
+    capabilities: [
+        {
+            os: "OS X",
+            os_version: "Big Sur",
+            browserName: "Safari",
+            browser_version: "latest",
+        },
+    ],
+};
+
+export const config = Object.assign({}, defaultConfig, overrides);
