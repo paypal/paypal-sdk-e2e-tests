17a94a154151e3aa36a991a330a820cd64c195bf
Nate Bierdeman <nbierdeman@paypal.com>
2022-06-09 10:39:43 -0700

    feat: set local testing default to false

--- resources/config/wdio.browserstack.conf.ts
+++ resources/config/wdio.browserstack.conf.ts
@@ -13,7 +13,7 @@ const overrides = {
         [
             "@browserstack/wdio-browserstack-service",
             {
-                browserstackLocal: true,
+                browserstackLocal: false,
             },
         ],
     ],
